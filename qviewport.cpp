5dbaa5669cd6b652cad97276bde1bf46b6b39a4c
Joao Guilherme Zeni <zenij@imag.fr>
2015-03-16 22:32:32 -0300

    Linking problem =(

--- qviewport.cpp
+++ qviewport.cpp
@@ -11,21 +11,36 @@ QViewport::~QViewport()
 void QViewport::paintEvent(QPaintEvent * ){
     painter.begin(this);
     painter.setBrush(QBrush(QColor(255.0, 255.0, 255.0)));
-    std::vector<DisplayFileObject>::iterator obj = objects->begin();
-    while(obj != obj.end()){
-        srd::string type = obj.getType();
+    std::vector<DisplayFileObject*> objs = objects->getObjects();
+    std::vector<DisplayFileObject*>::iterator obj = objs.begin();
+    for(int i = 0; i < objs.size(); i++){
+        std::string type = obj[i]->getType();
         if(type == "point"){
-        	Coordinate c = obj.getCoordinates();
-        	painter.drawPoint( c.x(), c.y());
+            Coordinate * c = obj[i]->getCoordinates()[0];
+            painter.drawPoint( c->x(), c->y());
         }
         else if(type == "line"){
-        	Coordinate c[2] = obj.getCoordinates();
-        	painter.drawLine( c[0].x(), c[0].y(), c[1].x(), c[1].y());
+            std::vector<Coordinate*> c = obj[i]->getCoordinates();
+            painter.drawLine( c[0]->x(), c[0]->y(), c[1]->x(), c[1]->y());
         }
         else{
             break;
         }
     }
+//    while(obj != objs.end()){
+//        srd::string type = obj.getType();
+//        if(type == "point"){
+//        	Coordinate c = obj.getCoordinates();
+//        	painter.drawPoint( c.x(), c.y());
+//        }
+//        else if(type == "line"){
+//        	Coordinate c[2] = obj.getCoordinates();
+//        	painter.drawLine( c[0].x(), c[0].y(), c[1].x(), c[1].y());
+//        }
+//        else{
+//            break;
+//        }
+//    }
 }
 
 void QViewport::updateObjects(DisplayFile * objs){
